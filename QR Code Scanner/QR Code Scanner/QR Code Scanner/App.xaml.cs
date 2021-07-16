d58f31ed18264cac51c4c3a7ad3bb8128eea5f8a
SvetoslavStefanov99 <87032543+SvetoslavStefanov99@users.noreply.github.com>
2021-07-16 18:20:30 +0300

    Finished project

---
+++ QR Code Scanner/QR Code Scanner/QR Code Scanner/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QR_Code_Scanner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new QRScanner();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
