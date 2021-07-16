a91a593b990fa98eb9c4721453efa30fe202c242
Heydzhan <87004627+Heydzhan@users.noreply.github.com>
2021-07-16 15:50:11 +0300

    Add files via upload

---
+++ QR Code Generator 2.0/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QR_Code_Generator_2._0
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            bCodeChange.BarcodeValue = entry.Text;
+        }
+    }
+}
