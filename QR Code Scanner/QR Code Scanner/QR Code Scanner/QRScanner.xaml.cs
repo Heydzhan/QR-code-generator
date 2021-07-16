bb544779822f2c35497632832126d5612524560c
SvetoslavStefanov99 <87032543+SvetoslavStefanov99@users.noreply.github.com>
2021-07-16 18:21:25 +0300

    Finished project

---
+++ QR Code Scanner/QR Code Scanner/QR Code Scanner/QRScanner.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QR_Code_Scanner
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QRScanner : ContentPage
+    {
+        public QRScanner()
+        {
+            InitializeComponent();
+        }
+        private void scanView_OnScanResult(ZXing.Result result)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                await DisplayAlert("Scanned Result", result.Text, "OK");
+            });
+        }
+    }
+}
